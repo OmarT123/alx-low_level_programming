b936205746cbe60ae3e04a71edf377988513dd3f
OmarT123 <omarabdelaty213@gmail.com>
2023-02-17 22:10:14 +0300

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,13 @@ int main(void)
 	c = 0;
 	while (c <= 9)
 	{
-		putchar(48 + c++);
+		putchar(48 + c);
 		if (c < 9)
 		{
 			putchar(',');
 		}
 		putchar(' ');
+		c++;
 	}
 	putchar('\n');
 	return (0);
