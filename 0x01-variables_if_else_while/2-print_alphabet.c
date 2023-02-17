fbd0d5ebb3d807d0ae3b3677c1c1d845e8ac7b4f
OmarT123 <omarabdelaty213@gmail.com>
2023-02-17 16:29:47 +0300

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (a <= 'z')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
