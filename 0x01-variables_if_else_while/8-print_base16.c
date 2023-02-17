55e714ecc66b1a5ce52ea727b690067a80dbb7c5
OmarT123 <omarabdelaty213@gmail.com>
2023-02-17 21:51:57 +0300

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+	char a;
+
+	i = '0';
+	a = 'a';
+	while (i <= 9)
+	{
+		putchar(48 + i++);
+	}
+	while (a <= 'f')
+	{
+		putchar(a++);
+	}
+	putchar('\n');
+	return (0);
+}
