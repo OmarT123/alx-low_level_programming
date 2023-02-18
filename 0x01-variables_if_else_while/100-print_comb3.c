b81cb507a81388bf5f8172e30de2d32b11983b9b
OmarT123 <omarabdelaty213@gmail.com>
2023-02-18 13:29:54 +0300

    2 numbers combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,12 @@
 
 int main(void)
 {
-	for (int i = 0; i < 9; i++)
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
 	{
-		for (int j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(48 + i);
 			putchar(48 + j);
