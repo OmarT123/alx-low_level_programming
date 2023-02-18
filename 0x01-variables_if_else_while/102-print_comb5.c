110b943639c92e0566a1aed9dc227ce55ff888f4
OmarT123 <omarabdelaty213@gmail.com>
2023-02-18 22:49:17 +0300

    Print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = 1; l <= 9; l++)
+				{
+					putchar(48 + i);
+					putchar(48 + j);
+					putchar(' ');
+					putchar(48 + k);
+					putchar(48 + l);
+					if (i != 9 || j != 9 || k != 9 || l != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
