85690c7898800f92a94f63359e7621b83d0981ef
OmarT123 <omarabdelaty213@gmail.com>
2023-02-18 22:29:17 +0300

    Print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (z = j + 1; z <= 9; z++)
+			{
+				putchar(48 + i);
+				putchar(48 + j);
+				putchar(48 + z);
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
