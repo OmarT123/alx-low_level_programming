baffea693077f41acf3b3712c9eb2226f737f80d
OmarT123 <omarabdelaty213@gmail.com>
2023-02-22 00:49:35 +0300

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 98)
+	if (n < 98)
 	{
 		for (i = n; i < 98; i++)
 		{
@@ -24,5 +24,5 @@ void print_to_98(int n)
 			printf("%d, ", i);
 		}
 	}
-	printf("98");
+	printf("98\n");
 }
