5a8755960a40f57a6604149e275a0a9a218bd3a6
OmarT123 <omarabdelaty213@gmail.com>
2023-02-27 22:17:19 +0300

    Print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * print_rev - prints string in reverse order
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	c--;
+	for (; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+
+}
