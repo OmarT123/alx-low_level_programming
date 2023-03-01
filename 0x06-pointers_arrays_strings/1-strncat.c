3c9b01665873367228e40cb01dd8cca60f9ef5d0
OmarT123 <omarabdelaty213@gmail.com>
2023-03-01 22:42:24 +0200

    Concat max n Strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include  "main.h"
+
+/**
+ * _strncat - concats n bytes of src to dest
+ * @dest: 1st string
+ * @src: 2nd string
+ * @n: Max bytes to be concatenated from src
+ * Return: String resulting from concatinating n bytes of src to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
