7221fe8ab90cffc2a93e470152678990da63a049
OmarT123 <omarabdelaty213@gmail.com>
2023-03-02 22:24:39 +0200

    Capitalize String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,10 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		while (!(s[i] >= 'a' && s[i] <= 'z'))
 			i++;
-		i--;
 		if (s[i - 1] == ' ' ||
 		s[i - 1] == '\t' ||
 		s[i - 1] == '\n' ||
