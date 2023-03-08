9a414e582a0ee3533289237d85ecbbc15481cd43
OmarT123 <omarabdelaty213@gmail.com>
2023-03-08 16:27:18 +0200

    Sqrt Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int root(int, int);
+
+/**
+ * _sqrt_recursion - calculates square root of number
+ * @n: input integer
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (n, n);
+}
+
+/**
+ * root - checks if r squared equals n
+ * @n: input int
+ * @r: guessed root of n
+ * Return: r if its the root, else -1
+ */
+
+int root(int n, int r)
+{
+	if (r * r == n)
+		return (r);
+	if (r * r < n)
+		return (-1);
+	return (root(n, r - 1));
+}
