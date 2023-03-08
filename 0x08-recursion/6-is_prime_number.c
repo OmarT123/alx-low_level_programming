e7adc1f8fc40eff34c395403e2e4b6e0db0b6a50
OmarT123 <omarabdelaty213@gmail.com>
2023-03-08 16:39:08 +0200

    Prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if n is prime
+ * @n: integer to check
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	return (eval(n, n));
+}
+
+/**
+ * eval - checks if n is prime
+ * @n: number to check if prime
+ * @r: guessed number
+ * Return: same as above
+ */
+
+int eval(int n, int r)
+{
+	if (r <= 0)
+		return (1);
+	if (n % r == 0)
+		return (0);
+	return (eval(n, r - 1));
+}
