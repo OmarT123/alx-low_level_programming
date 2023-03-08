bc1f68999a0fa63e617ba2661a2195f45e163200
OmarT123 <omarabdelaty213@gmail.com>
2023-03-08 18:14:50 +0200

    Plaindrome

---
+++ 0x08-recursion/100-ispalindrome.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if string is palindrome
+ * @s: input string
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int i;
+
+	while (s[i])
+	{
+		i++;
+	}
+	i--;
+	return (ispal(s, 0, i));
+}
+
+/**
+ * ispal - checks if string is palindrome
+ * @s: input string
+ * @f: first letter of string
+ * @i: pointer to last character of string
+ * Return: same as is_palindrome
+ */
+
+int ispal(char *s, int f, int i)
+{
+	if (i >= f)
+		return (1);
+	if (s[i] != s[f])
+		return (0);
+	i--;
+	f++;
+	return (ispal(s, f, i));
+}
