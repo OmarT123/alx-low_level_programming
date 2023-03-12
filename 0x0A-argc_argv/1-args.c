9b844ed52dc1d1617eee322e159dc0c2d298dd0a
OmarT123 <omarabdelaty213@gmail.com>
2023-03-12 15:10:09 +0100

    Size of input

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: length of input
+ * @argv: input array
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
