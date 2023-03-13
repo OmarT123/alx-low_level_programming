ae009bb8c96bad743824a640f621428dc6832f2d
OmarT123 <omarabdelaty213@gmail.com>
2023-03-14 00:18:53 +0100

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,8 @@ char *create_array(unsigned int size, char c)
 		return (0);
 	}
 	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (0);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
