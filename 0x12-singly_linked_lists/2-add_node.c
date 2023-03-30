52d52054b04312aaf360b16e27dd74adc4fa488c
OmarT123 <omarabdelaty213@gmail.com>
2023-03-30 17:11:58 +0200

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *temp;
 
-	temp = malloc (sizeof(list_t));
+	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
 	temp->len = strlen(str);
