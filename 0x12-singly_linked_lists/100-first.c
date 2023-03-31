65624c2ca906775bf0142a5a62af14b8be973a5d
OmarT123 <omarabdelaty213@gmail.com>
2023-03-31 16:01:54 +0200

    function before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,13 +6,13 @@
  * before main()
  */
 
-void startFunc(void) __attribute__ ((const));
+void startFunc(void);
 
 /**
  * startFunc - function that executes before main method
  */
 
-void startFunc(void)
+void startFunc(void) __attribute__ ((constructor))
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
