f1503bd22b6bc113f4e3418f2d9392dc10969466
OmarT123 <omarabdelaty213@gmail.com>
2023-03-31 15:58:28 +0200

    function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * startFunc - add constructor attribute to startFun() so it executes
+ * before main()
+ */
+
+void startFunc(void) __attribute__ ((constructor));
+
+/**
+ * startFunc - function that executes before main method
+ */
+
+void startFunc(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
