396cb1c6c25a5b9402ef37c428eb1f59527f8c6e
OmarT123 <omarabdelaty213@gmail.com>
2023-04-04 13:43:14 +0200

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	cur = *head;
 	for (i = 0; i < (index - 1); i++)
 	{
-		if (cur == NULL || curr->next == NULL)
+		if (cur == NULL || cur->next == NULL)
 			return (-1);
 		cur = cur->next;
 	}
